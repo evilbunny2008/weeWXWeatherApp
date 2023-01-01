f47eb86f410c61bfe72ffb623827663a4f144ba8
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2023-01-01 14:33:16 +1100

    Upstreamed changes

--- app/src/main/java/com/odiousapps/weewxweather/WidgetProvider.java
+++ app/src/main/java/com/odiousapps/weewxweather/WidgetProvider.java
@@ -5,7 +5,6 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.widget.RemoteViews;
 
 public class WidgetProvider extends AppWidgetProvider
